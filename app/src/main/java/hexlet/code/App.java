6c4b6a5faf1eec898fa39a4c77c16fe8cf02ebe0
Constantine <77935386+velteren@users.noreply.github.com>
2024-02-02 17:34:15 +0300

    feat: implement subtask 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,8 +5,16 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "app", version = "1.0", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", version = "1.0", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to first file")
+    private String filepath2;
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
