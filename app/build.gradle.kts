5f31e37662d95286f3b56993fbeb1f59b47ffdaf
Constantine <77935386+velteren@users.noreply.github.com>
2024-02-02 16:32:53 +0300

    feat: subtask 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -17,3 +18,10 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
