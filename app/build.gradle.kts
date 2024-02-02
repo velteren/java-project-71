cb6415ffcec12ff3cc12a32f040b8fcf8c6be673
Constantine <77935386+velteren@users.noreply.github.com>
2024-02-02 17:13:42 +0300

    feat: implement subtask 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.5")
 }
 
 tasks.test {
